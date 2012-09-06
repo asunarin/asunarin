ec1a65984768e0adf017e6ce760e113fe0bda7a0
asunarin <asuna@yopmail.com>
2012-09-06 16:34:35 +0800

    22

--- temp.php
+++ temp.php
@@ -1,6 +1,6 @@
 <?php
-print sys_get_temp_dir().
-print sys_get_temp_dir
-print  $_ENV['sys_get_temp_dir()']
-print  $_ENV['sys_get_temp_dir']
+print sys_get_temp_dir();
+print sys_get_temp_dir;
+print  $_ENV['sys_get_temp_dir()'];
+print  $_ENV['sys_get_temp_dir'];
 ?>
